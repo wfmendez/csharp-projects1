56cb923b6e2345ef845d4fedb1ccb3285e4889f6
wfmendez <wuillian.f.mendez@gmail.com>
2023-04-21 21:11:26 -0400

    Init Preps

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("This is in C#.");
+
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,34 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.WriteLine("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput)
+
+        if (grade >= 90)
+        {
+            Console.WriteLine("A");
+        }
+
+        else if (grade >= 80)
+        {
+            Console.WriteLine("B");
+        }
+
+        else if (grade >= 70)
+        {
+            Console.WriteLine("B");
+        }
+
+        else if (grade >= 60)
+        {
+            Console.WriteLine("B");
+        }
+
+        else 
+        {
+            Console.WriteLine("F");
+        }
     }
 }
